4c890199b00a4a560f010676638d161a749e0e06
keryiing27 <81569360+keryiing27@users.noreply.github.com>
2021-04-05 12:20:40 +0800

    Update Dog.java

--- Example2/Dog.java
+++ Example2/Dog.java
@@ -1,11 +1,14 @@
 package Example2;
 
-public class Dog {
+public class Animal {
 	
 	String dogName, eyeColor, breed;
 	int dogAge;
 	String bark;
 	String eat;
+	String catName, plays;
+	int catAge, giveBirth;
+	
 	void changeName() {
 		System.out.println("Another doggy name: " + dogName);
 	}
@@ -21,4 +24,19 @@ public class Dog {
 	void printEat() {
 		System.out.println(eat);
 	}
+	
+	void printcatAge() {
+		System.out.println("Cat age: " + catAge + " years old");
+	}
+	
+	void printplays() {
+		plays = "toys";
+		System.out.println("It likes to play " + plays);
+	}
+	
+	void printGiveBirth() {
+		System.out.println("A cat can give birth to an average of " + giveBirth + " kittens.");
+				
+	}
+
 }
